36fe4da49e2ed9fcd18cd4fbfa7693966e11ec18
Martin Michálek <michalek@shortcat.cz>
2012-10-05 14:31:13 +0200

    Bootstrap + Sablona

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="cs">
+  
+  <head>
+    <meta charset="utf-8">
+    <title>Server side reponsive test</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    
+    <style>
+      body { padding-top: 60px; /* 60px to make the container go all the way
+      to the bottom of the topbar */ }
+    </style>
+    
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+  </head>
+  
+  <body>
+    <div class="navbar navbar-fixed-top navbar-inverse">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="#">
+            Project name
+          </a>
+          <ul class="nav">
+            <li>
+              <a href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="hero-unit">
+        <div>
+          <h1>
+            Ahoj!
+          </h1>
+          <p>
+            Sleduji OVM, abych se ujistil, že lidé, o kterých si myslím, že jsou ministři,
+            jsou ještě ministři. Nenechám si ujít ani zasvěcené diskuze zajímavých
+            hostů a odborníků v Historii.cs.
+          </p>
+        </div>
+        <a class="btn btn-primary" href="#">
+          Learn more »
+        </a>
+      </div>
+      <div class="row">
+        <div class="span4">
+          <div>
+            <h2>
+              Letiště JFK, de Gaulla - a teď i Václava Havla
+            </h2>
+            <p>
+              Bydlel tu letí víkem funěla bratra. Oba kase smutno vyl do devětkrát ceny.
+            </p>
+          </div>
+          <a class="btn" href="#">
+            View details »
+          </a>
+        </div>
+        <div class="span4">
+          <div>
+            <h2>
+              Dobrodružství vědy a techniky
+            </h2>
+            <p>
+              Sleduji OVM, abych se ujistil, že lidé, o kterých si myslím, že jsou ministři,
+              jsou ještě ministři. Nenechám si ujít ani zasvěcené diskuze zajímavých
+              hostů a odborníků v Historii.cs.
+            </p>
+          </div>
+          <a class="btn" href="#">
+            View details »
+          </a>
+        </div>
+        <div class="span4">
+          <div>
+            <h2>
+              Cesta do lesa
+            </h2>
+            <p>
+              Bydlel tu letí víkem funěla bratra. Oba kase smutno vyl do devětkrát ceny,
+              o mém bůhvíjak papoušek podrobnosti: večer vyvolá au vezl s tobě nebezpečný
+              81, titul plané zabručel.&nbsp;Bydlel tu letí víkem funěla bratra.&nbsp;
+            </p>
+            <p>
+            </p>
+          </div>
+          <a class="btn" href="#">
+            View details »
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="span12">
+          <h3>
+            Obsah co není vidět na mobilech
+          </h3>
+          <div>
+            Sleduji OVM, abych se ujistil, že lidé, o kterých si myslím, že jsou ministři,
+            jsou ještě ministři. Nenechám si ujít ani zasvěcené diskuze zajímavých
+            hostů a odborníků v Historii.cs.
+            <br>
+          </div>
+          <div>
+            <br>
+          </div>
+          <div>
+            Bydlel tu letí víkem funěla bratra. Oba kase smutno vyl do devětkrát ceny,
+            o mém bůhvíjak papoušek podrobnosti: večer vyvolá au vezl s tobě nebezpečný
+            81, titul plané zabručel.
+            <br>
+          </div>
+          <div>
+            <br>
+          </div>
+          <h3>
+          </h3>
+        </div>
+      </div>
+      <hr>
+      <div>
+        © Company 2012
+      </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">
+    </script>
+  </body>
+
+</html>
